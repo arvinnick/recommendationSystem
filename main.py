3c3e0e68b03f55caf61a8c6075dbe62a39f10d67
Arvin <arvin.nick.90@gmail.com>
2024-08-10 00:07:41 +0400

    created generator

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
+import asyncio
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+from random import randint
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.post("/generator")
+async def generator(model_name):
+    """
+    The service that generates recommendations
+    :param payload: json payload including model name and viewer id
+    :return: random recommendation. A json data including reason (which is essencially the model name)
+             and result (recommended number)
+    """
+    random_number = randint(1, 100)
+    return {"reason": model_name, "result": random_number}
+
+
+
+
+
